140e4bca4be98ce49aa001affcb79fe9a2cd945a
MARJORY DE SOUSA VILLAS BOAS <26171862@edu119.local>
2026-09-24 15:48:31 -0300

    Fix

---
+++ readme.md
@@ -0,0 +1,35 @@
+# Campeonato Gamer
+
+## Situação Problema 
+
+Nossa turma vai organizar partidas de jogos. Queremos registrar **Jogo**, **time A**,  **time B**,  **placar** e  **status**. Uma partida começa `agenda` com placar `0 X 0`. Depois podemos atualizar o resultado para `finalizar`.
+
+Ao final, a API terá estas rotas
+
+| Método  | Rota    |   O que faz   | 
+| ---     | ---     |   ---
+| GET     | `/partidas`     |   Confirma que a API está funcionando|
+| GET     | `/partidas`      |  Lista e filtra partidas|
+| GET        | `/partidas`       | Buscar uma partida|
+| POST        | `/partidas`       | Alterar uma partida e o placar|
+| PUT       | `/partidas`       | Excluir uma partida|
+| DELETE        | `/partidas`       |
+
+
+## Etapa 1 - Ciar o projeto
+
+No terminal, digite **uma linha por vez**;
+
+```bash
+    mkdir CampeonatoGamer
+    cd CampeonatoGamer
+    npm init -y
+    npm install express cors
+    code . 
+```"# CampeonatoGamer" 
+"# CampeonatoGamer" 
+"# CampeonatoGamer" 
+"# CampeonatoGamer" 
+"# CampeonatoGamer" 
+"# CampeonatoGamer" 
+"# CampeonatoGamer" 
---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors()); //middleware
+app.use(express.json());
+
+let PARTIDAS = [
+    {
+        id: 1,
+        jogo: "Arena Pixel",
+        timeA: "Falcões",
+        timeB: "Dragões",
+        pontoA: 1,
+        pontoB: 2,
+        status: "Finalizado"
+
+    },
+    {
+        id: 1,
+        jogo: "Corrida Turbo",
+        timeA: "Lobos",
+        timeB: "Falcões",
+        pontoA: 0,
+        pontoB: 0,
+        status: "agendada"
+
+    },
+];
+
+
+app.get("/",(req, res)=>{
+    res.status(200).json({mensagem: "Campeonato Gamer no ar!"})
+});
+app.get("/partidas",(req, res)=>{
+    res.status(200).json(PARTIDAS);
+});    
+
+app.get("/partidas/:id",(req, res)=>{
+    const id = Number(req.params.id);
+    const partida = PARTIDAS.find(p => p.id === id);
+
+    if(!partida) return res.status(404).json({mensagem: "Partida não encontrada"});
+
+    res.status(200).json(partida);
+});
+
+app.post("/partidas",(req, res)=>{
+    const {jogo, timeA, timeB} = req.body;
+
+    if(!jogo || !timeA || !timeB){
+        return res.status(400).json({
+            mensagem: "Informe jogo, timeA e timeB"
+        });
+    }
+
+    if(timeA.trim().toLowerCase() === timeB.trim().toLowerCase){
+        return res.status(400).json({
+            mensagem: "Os times devem ser diferentes"
+        });
+    }
+    const novoId = PARTIDAS.length > 0
+        ?Math.max(...PARTIDAS.map(p =>p.id)) +1 : 1;
+
+    const novaPartida ={
+        id: novoId,
+        jogo: jogo.trim(),
+        timeA: timeA.trim(),
+        timeB: timeB.trim(),
+        pontoA: 0,
+        pontoB: 0,
+        status: "agendada",
+    }
+
+PARTIDAS.push(novaPartida);
+res.status(201).json({
+    mensagem: "Partida Cadastrada",
+    partida: novaPartida
+
+});
+
+});
+
+const PORT = 3000;
+app.listen(PORT, ()=>{
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
